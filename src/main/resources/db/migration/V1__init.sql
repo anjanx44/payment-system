b1005fa31669baee0abd90158aa1562504eedce3
anjanx44 <anjanx44@gmail.com>
2025-01-24 17:05:21 +0600

    flyway migration done

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,9 +1,11 @@
-CREATE TABLE payment
-(
-    id         UUID PRIMARY KEY,
-    request_id VARCHAR(255),
-    amount     DECIMAL(19, 2),
-    currency   VARCHAR(3),
-    status     VARCHAR(255),
-    provider   VARCHAR(255)
-);
+CREATE TABLE IF NOT EXISTS payments (
+    id UUID PRIMARY KEY,
+    amount DECIMAL(15, 2) NOT NULL,
+    currency VARCHAR(10) NOT NULL,
+    customer_id VARCHAR(100) NOT NULL,
+    country VARCHAR(50) NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    provider VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+    );
